0350395169a6b4f3c159659cc9d3b89b5ff9e911
Daniimazo <Daniel60mv@gmail.com>
2025-03-25 10:19:36 -0500

    Se crea el metodo constructor en la clase Reserva, asi tambien se le da validaciones

--- Reserva.cs
+++ Reserva.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace wGestionReservasHotel
 {
-    internal class Reserva
+    public abstract class Reserva
     {
+        public string NombreCliente { get; set; }
+        public int NumeroHabitacion { get; set; }
+        public DateTime FechaReserva { get; set; }
+        public int DuracionEstadia { get; set; }
+
+        public Reserva(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracion)
+        {
+            if (string.IsNullOrWhiteSpace(nombreCliente))
+                throw new ArgumentException("El nombre del cliente es obligatorio.");
+            if (duracion < 1)
+                throw new ArgumentException("La duración de la estadía debe ser mayor a 1 noche.");
+
+            NombreCliente = nombreCliente;
+            NumeroHabitacion = numeroHabitacion;
+            FechaReserva = fechaReserva;
+            DuracionEstadia = duracion;
+        }
+
+        public abstract decimal CalcularCostoTotal();
     }
+
 }
