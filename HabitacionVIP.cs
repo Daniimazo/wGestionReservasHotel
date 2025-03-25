60599d65f4a6fb48fe42da111fede79d592bdaa3
Daniimazo <Daniel60mv@gmail.com>
2025-03-25 10:31:28 -0500

    Se hace el metodo constructor de HabitacionVIP y se le agregan validaciones

--- HabitacionVIP.cs
+++ HabitacionVIP.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace wGestionReservasHotel
 {
-    internal class HabitacionVIP
+    public class HabitacionVIP : Reserva
     {
+        public decimal TarifaBase { get; set; }
+        public const decimal Descuento = 0.20m;
+
+        public HabitacionVIP(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracion, decimal tarifaBase)
+            : base(nombreCliente, numeroHabitacion, fechaReserva, duracion)
+        {
+            if (tarifaBase <= 0)
+                throw new ArgumentException("La tarifa base no puede ser menor o igual a cero.");
+
+            TarifaBase = tarifaBase;
+        }
+
+        public override decimal CalcularCostoTotal()
+        {
+            decimal total = TarifaBase * DuracionEstadia;
+            return DuracionEstadia > 5 ? total * (1 - Descuento) : total;
+        }
     }
+
 }
