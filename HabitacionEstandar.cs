918db3893139acbf456f74e8ee1b43b20c66ed37
Daniimazo <Daniel60mv@gmail.com>
2025-03-25 10:23:24 -0500

    Se hace el metodo constructor a la clase HabittacionEstandar

--- HabitacionEstandar.cs
+++ HabitacionEstandar.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace wGestionReservasHotel
 {
-    internal class HabitacionEstandar
+    public class HabitacionEstandar : Reserva
     {
+        public decimal TarifaFija { get; set; }
+
+        public HabitacionEstandar(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracion, decimal tarifa)
+            : base(nombreCliente, numeroHabitacion, fechaReserva, duracion)
+        {
+            if (tarifa <= 0)
+                throw new ArgumentException("La tarifa no puede ser menor o igual a cero.");
+
+            TarifaFija = tarifa;
+        }
+
+        public override decimal CalcularCostoTotal()
+        {
+            return TarifaFija * DuracionEstadia;
+        }
     }
+
 }
