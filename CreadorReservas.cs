ac1076405d97af5900e5ac96d2eac0c738f722e2
Daniimazo <Daniel60mv@gmail.com>
2025-03-25 10:48:49 -0500

    Se le agrega validaciones para que cree la reserva segun la habitacion

--- CreadorReservas.cs
+++ CreadorReservas.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace wGestionReservasHotel
 {
-    internal class CreadorReservas
+    public class CreadorReservas
     {
+        public static Reserva CrearReserva(string tipo, string nombre, int numHabitacion, DateTime fecha, int duracion, decimal tarifa)
+        {
+            if (tipo == "Estandar")
+            {
+                return new HabitacionEstandar(nombre, numHabitacion, fecha, duracion, tarifa);
+            }
+            else if (tipo == "VIP")
+            {
+                return new HabitacionVIP(nombre, numHabitacion, fecha, duracion, tarifa);
+            }
+            else
+            {
+                throw new Exception("Tipo de reserva no válido.");
+            }
+        }
     }
+
 }
